defdb9d144b15848c46527d0bead0122f8c3566c
s.goduyko <stanislav.goduyko@gmail.com>
2025-02-26 02:53:39 +0300

    work experience section. TFN and UnicLab

--- locales/en.js
+++ locales/en.js
@@ -24,4 +24,32 @@ const enTranslate = {
   "professional-activity-broker-word": "Broker Queue:",
   "professional-activity-devops-word": "DevOps:",
   "professional-activity-git-word": "Version Control System:",
+  "work-experience-title-name": "Work Experience",
+  "tasks-and-achievements": "Tasks and achievements",
+  "job-item-description-and-role-title-name": "Description of the project/role",
+  "job-item-description-and-role-tech-name": "Технологический стэк",
+
+  // TFN company
+  "tfn-task-item-1": "Optimization SQL-queries",
+  "tfn-task-item-2": "Improvements in payment processes on the service",
+  "tfn-task-item-3": "Improvements in the registration process for new devices, etc .",
+  "tfn-task-item-4": "Acceleration of responses from the service",
+  "tfn-task-item-5": "Improvement and correction of functionality",
+  "tfn-task-item-6": "Improvement of push notifications and email dispatch with the ability to manage and monitor the general dispatch",
+  "tfn-task-item-7": "Responding to problems with the service",
+  "tfn-task-item-8": "Fix errors",
+  "tfn-task-item-9": "Code review",
+  "tfn-task-item-10": "Write documentation (C4)",
+  "tfn-task-item-11": "Pipelines for extracting data from files to a database",
+  "tfn-description-and-role": "a telematic system of a car. The system was an existing, work was underway to fix bugs and speed up bottlenecks. At the same time, the functionality on all services was regularly improved/added.",
+
+  //  UnicLab company
+  "uniclab-task-item-1": "Code review",
+  "uniclab-task-item-2": "Database design",
+  "uniclab-task-item-3": "Architectural solutions",
+  "uniclab-task-item-4": "Write documentation (C4)",
+  "uniclab-task-item-5": "Create ERIP reports for smart meters",
+  "uniclab-task-item-6": "Sync data between the data aggregator and the data analytics and display service",
+  "uniclab-task-item-7": "Development of the address/location module",
+  "uniclab-description-and-role": "Smart meter data aggregation system. Smart meters for gas, water and electricity. A personal data service has been developed that deals with data display and analysis. I went all the way with the project from the beginning of the development of the personal account service to the final release of the entire system in a production environment."
 }
