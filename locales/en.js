c6fffe20719ee5abcfbe1899c5dc6e915eb79971
s.goduyko <stanislav.goduyko@gmail.com>
2025-02-25 23:07:25 +0300

    section of education, projects, rps and languages

--- locales/en.js
+++ locales/en.js
@@ -1,9 +1,19 @@
 const enTranslate = {
   "fio": "Stanislav Goduyko",
   "current-position": "Python/Go Backend Developer",
-  "about-yourself-letter": "I have been working on projects that daily help tens of thousands of people. I have been working" +
-      "independently and in a development team. I came to an existing project and worked on the" +
+  "about-yourself-letter": "I have been working on projects that daily help tens of thousands of people. I have been working " +
+      "independently and in a development team. I came to an existing project and worked on the " +
       "development of a service from scratch.\n" +
-      "In free time I play football, gather friends for board games and go out into nature. I like to travel" +
+      "In free time I play football, gather friends for board games and go out into nature. I like to travel " +
       "and meet new people. Professionally falling off a snowboard",
+  "cv-header-education-title-name": "Education",
+  "university-name": "Brest State A.S.Pushkin University",
+  "university-specialization-name": "Software Engineer",
+  "cv-header-projects-title-name": "Projects",
+  "scherkhan-project-short-description": "Car telematics service",
+  "uniclab-project-short-description": "Smart meter data aggregation and display data services",
+  "schoolsby-project-short-description": "Mobile applications for school platform",
+  "cv-header-languages-title-name": "Languages",
+  "cv-header-languages-en": "English (B1)",
+  "cv-header-languages-ru": "Russian (native speaker)",
 }
